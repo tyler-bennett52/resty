8025553e2b6620365c736108616c0b2f518a48fa
TYLER BENNETT <tyler.bennett52@gmail.com>
2023-03-19 05:22:10 -0500

    refactor to useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import axios from 'axios';
 
 import './App.scss';
@@ -8,36 +8,84 @@ import Footer from './Components/Footer';
 import Form from './Components/Form';
 import Results from './Components/Results';
 
+const initialState = {
+  url: 'https://swapi.dev/api/',
+  method: 'GET',
+  data: null,
+  loading: false,
+  response: null,
+  history: []
+}
 
-function App(props) {
-  const [response, setResponse] = useState(null);
-  const [requestParams, setRequestParams] = useState({url: 'https://swapi.dev/api/', method: 'GET', data: null});
-  const [loading, setLoading] = useState(false);
-  
+const actionList = {
+  changeUrl: 'change-url',
+  changeMethod: 'change-method',
+  changeBody: 'change-body',
+  storeResponse: 'store-response',
+  addHistory: 'add-history',
+  responseFromHistory: 'response-from-history',
+  loadingToggle: 'loading-toggle'
+}
 
-  const callApi = async (requestParams) => {
-    // mock output
+function requestReducer(state = initialState, action) {
+  switch (action.type) {
+
+    case 'change-url':
+      return { ...state, url: action.payload }
+
+    case 'change-method':
+      return { ...state, method: action.payload }
+
+      case 'change-body':
+      return { ...state, data: action.payload }
+
+    case 'store-response':
+      return { ...state, response: action.payload }
+
+    case 'loading-toggle':
+      return {...state, loading: !state.loading}
+
+    case 'add-history':
+      state.history.push(action.payload);
+      localStorage.setItem('history', JSON.stringify(state.history));
+      return state
+
+    case 'response-from-history':
+      state.response = state.history[action.payload]
+      return state
+
+    default:
+      break;
+  }
+}
+
+
+function App() {
+
+  const [state, dispatch] = useReducer(requestReducer, initialState);
+  const callApi = async () => {
     try {
-      setLoading(true)
-      const response = await axios({...requestParams, data: JSON.parse(requestParams.data)});
+      dispatch({ type: actionList.loadingToggle });
+      const requestParams = { url: state.url, method: state.method };
+      if (state.data) requestParams.data = JSON.parse(state.data);
+      const response = await axios(requestParams);
       await new Promise(resolve => setTimeout(resolve, 500))
-      setResponse(response);
-      setRequestParams(requestParams);
-      setLoading(false);
+      dispatch({ type: actionList.storeResponse, payload: response });
+      dispatch({ type: actionList.loadingToggle })
     } catch (error) {
-      console.log(error)
-      setLoading(false);
+      console.log(error);
+      dispatch({ type: actionList.loadingToggle });
     }
   }
 
   return (
     <>
       <Header />
-      <div style={{ textAlign: "center" }}>Request Method: {requestParams.method}</div>
-      <div style={{ textAlign: "center" }}>URL: {requestParams.url}</div>
-      <div style={{ textAlign: "center" }}>Request Body: {requestParams.data}</div>
-      <Form handleApiCall={callApi} setRequestParams={setRequestParams} requestParams={requestParams} />
-      <Results response={response} loading={loading} />
+      <div style={{ textAlign: "center" }}>Request Method: {state.method}</div>
+      <div style={{ textAlign: "center" }}>URL: {state.url}</div>
+      <div style={{ textAlign: "center" }}>Request Body: {state.data}</div>
+      <Form handleApiCall={callApi} dispatch={dispatch} requestParams={state} actionList={actionList} />
+      <Results response={state.response} loading={state.loading} />
       <Footer />
     </>
   );
